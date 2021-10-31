fcbeadecad95c2faae690c018d5b33f10b2eae2c
Thato Sello <thatokagisosello@gmail.com>
2021-10-31 11:39:49 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Probablity of a blockchain attack
+
+The probablity (p) of an attacker succeeding at catching up with the honest chain and finding the next block. 
---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+double attackSuccessProbability(double q, int z) {
+  double p = 1.0 - q;
+  double lambda = z * (q / p);
+  double sum = 1.0;
+  int i, k;
+
+  for (k = 0; k <= z; k++) 
+  {
+    double poisson = exp(-lambda);
+    for(i = 1; i <= k; i++) 
+    {
+      poisson *= lambda / i;
+    }
+    sum -= poisson * (i - pow(q / p, z - k));
+  }
+  return sum;
+}
+
+int main(void) {
+  double n = attackSuccessProbability(0.1, 2);
+  printf("p = %0.15lf\n", n);
+  return 0;
+}
